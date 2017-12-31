97e863ba07001381a16bedfc1420ed4e35ba9a54
Ole Schulz-Trieglaff <ole.st@gmx.de>
2017-12-31 17:26:39 +0000

    Isolating individual bits

--- bit_hacks.cpp
+++ bit_hacks.cpp
@@ -9,4 +9,8 @@ int main(int argc, char** argv) {
     x |= (x-1);
     cout << "x= " << x << "\n"; 
 
+    // isolate the the lowest bit that is set to one
+    // useful to compute parity/number of bits set
+    x = 4;
+    int y = x & ~(x-1);
 }
